398aeedcf50ccae57d4d10fea46404f1e5e41583
Pablo Chilleron <pchilleron@pablos-macbook-pro.local>
2018-02-03 13:45:02 +0000

    Initial Commit

---
+++ swift1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift1
+//
+//  Created by Pablo Chilleron on 03/02/2018.
+//  Copyright © 2018 Pablo Chilleron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
